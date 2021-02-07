c64874ca1570d0f2d51d7e34a927a677ade95fc2
Ambush9 <ambush2626@gmail.com>
2021-02-07 21:01:54 +0300

    basic routes, migrations and models for categories

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/', MainController::class);
+Route::get('/categories', [MainController::class, 'categories']);
+Route::get('/{category}', [MainController::class, 'category']);
+
+Route::get('/mobiles/{product?}', [MainController::class, 'product']);
