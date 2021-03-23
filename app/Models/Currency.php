9303b24ed9a8fdfbef431da83b1bea438ce38c54
Ambush9 <ambush2626@gmail.com>
2021-03-23 18:22:11 +0300

    Lesson 29. Added different currencies, but didnt finished.

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -8,9 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Currency extends Model
 {
     use HasFactory;
+    protected $fillable = ['rate'];
 
     public function scopeByCode($query, $code)
     {
         return $query->where('code', $code);
+    }
+
+    public function isMain()
+    {
+        return $this->is_main === 1;
+
+        // TODO
     }
 }
--- app/Services/CurrencyConversion.php
+++ app/Services/CurrencyConversion.php
@@ -5,6 +5,7 @@ namespace App\Services;
 
 
 use App\Models\Currency;
+use Carbon\Carbon;
 
 class CurrencyConversion
 {
@@ -12,17 +13,35 @@ class CurrencyConversion
     public static function convert($sum, $originCurrencyCode='RUB', $targetCurrencyCode = null)
     {
         $originCurrency = Currency::byCode($originCurrencyCode)->first();
+        // проверяем что курс обновлялся сегодня
+
+        if ($originCurrency->updated_at->startOfDay() != Carbon::now()->startOfDay()) {
+            CurrencyRates::getRates();
+//            self::loadContainer();
+            $originCurrency = Currency::byCode($originCurrencyCode)->first();
+        }
+
         if(is_null($targetCurrencyCode)) {
             $targetCurrencyCode = session('currency','RUB');
         }
         $targetCurrency = Currency::byCode($targetCurrencyCode)->first();
+        if ($targetCurrency->updated_at->startOfDay() != Carbon::now()->startOfDay()) {
+            CurrencyRates::getRates();
+//            self::loadContainer();
+            $targetCurrency = Currency::byCode($targetCurrencyCode)->first();
+        }
 
-        return $sum * $originCurrency->rate / $targetCurrency->rate;
+        return $sum / $originCurrency->rate * $targetCurrency->rate;
     }
 
     public static function getCurrencySymbol()
     {
         $currency = Currency::byCode(session('currency', 'RUB'))->first();
         return $currency->symbol;
+    }
+
+    public static function getBaseCurrency()
+    {
+        // TODO
     }
 }
